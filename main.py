caa45d14857cd73045ac43e881006fd7ea4c3094
Dmitrii058 <oootrakindustria@gmail.com>
2026-03-25 20:24:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-number = int(input("Введите число: "))
-if number > 0:
-    print("Число положительное")
-elif number < 0:
-    print("Число отрицательное")
-else:
-    print("Это ноль")
-
-print ("Hello, world!")
+print ("Как дела")
